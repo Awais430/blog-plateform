47c7677f4a8f13a2d5dc4414ebe9bc9f91deb056
unknown <hamzaaliics@gmail.com>
2024-02-05 19:45:45 +0500

    Your commit message here

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import {BrowserRouter,Route,Routes} from "react-router-dom"
 import "./App.css"
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Project from './pages/Project'
+
 const App = () => {
   return (
-    <div className=''>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route  path='/about' element={<About/>}/>
+      <Route  path='/sign-in' element={<SignIn/>}/>
+      <Route  path='/sign-up' element={<SignUp/>}/>
+      <Route  path='/projects' element={<Project/>}/>
+      {/* <Route  path='/' element={<About/>}/> */}
+
+    </Routes>
+    </BrowserRouter>
   )
 }
 
